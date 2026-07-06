eee1ca17db95ee3f46fd01cbb51dc3332eaaf69f
GokceSoylu <gokcesoylu24@gmail.com>
2026-07-06 09:10:42 +0300

    config-projectg created

---
+++ Dependency_Injections/colours-config/src/main/java/com/gokce/colours_config/ColoursConfigApplication.java
@@ -0,0 +1,13 @@
+package com.gokce.colours_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoursConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoursConfigApplication.class, args);
+	}
+
+}
